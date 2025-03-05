f6b29f341651d4d5406383b248b04d2e465fd471
Anastasios Moraitis <tasosthira7@gmail.com>
2025-03-05 20:44:21 +0000

    About us and theme

--- scripts.js
+++ scripts.js
@@ -10,3 +10,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const defaultLang = userLang.startsWith('el') ? 'el' : 'en';
     setLanguage(defaultLang);
 });
+
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
